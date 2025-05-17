64fd398fdfe3e3edca184fc4d5d18470e2c4d6b9
Zuxx <40512201+zuxxefied@users.noreply.github.com>
2025-05-17 18:03:24 +0530

    Updated The mobile texts and fixed the work catagory images and fixed home icon for Advertisments

--- work/work.js
+++ work/work.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const scrambleTitle = document.getElementById("scramble-title");
   if (scrambleTitle) {
     const original = scrambleTitle.textContent;
-    const chars = "_______%";
+    const chars = "____A___B___C___D___E___F___G___H___I___J___K___L___M___N___O___P___Q___R___S___T___U___V___W___X___Y___Z___a___b___c___d___e___f___g___h___i___j___k___l___m___n___o___p___q___r___s___t___u___v___w___x___y___z___0___1___2___3___4_____5___6___7___8___9";
     let interval = null;
 
     scrambleTitle.addEventListener("mouseenter", () => {
       let iteration = 0;
-      clearInterval(interval);
+      clearInterval(interval);  
 
       interval = setInterval(() => {
         scrambleTitle.textContent = original
